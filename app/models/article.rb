ad185ca305f8e0a0390d79dc9e3528daa7e6c7e1
3rick <erick.dejesus1@alumno.buap.mx>
2022-11-06 15:31:21 -0600

    add article validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  # title must be present
+  validates :title, presence: true
+  # body must be present
+  # and be at least 10 characters long
+  validates :body, presence: true, length: { minimum: 10}
 end

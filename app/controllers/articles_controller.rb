d612b21d82a2e39ef375a28c929c40b15fd21954
3rick <erick.dejesus1@alumno.buap.mx>
2022-11-06 14:48:13 -0600

    new action: articles #new, #create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,26 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  # create but does not save it
+  # useful in the view
+  def new
+    # render
+    # app/view/articles/new.html.erb
+    @article = Article.new
+  end
+  # instantiate a new article and attempts to save it
+  def create
+    # dummy values for title and body
+    # todo: change for form values
+    @article = Article.new(title:"...",body: "...")
+    if @article.save
+      #localhost:port/articles/#{@article.id}
+      redirect_to @article
+    else
+      #render form
+      # app/views/articles/new.html.erb
+      # with status code 422
+      render :new, status: :unprocessable_entity
+    end
+  end
 end

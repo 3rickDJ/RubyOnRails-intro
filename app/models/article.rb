777651d21c9da8b2496334d30dc65227b7618bef
3rick <erick.dejesus1@alumno.buap.mx>
2022-12-01 18:09:53 -0600

    refactor: validation status in article using visble concern

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,16 +1,10 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
   # title must be present
   validates :title, presence: true
   # body must be present
   # and be at least 10 characters long
   validates :body, presence: true, length: { minimum: 10}
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES}
-
-  def archived?
-    status == 'archived'
-  end
 end

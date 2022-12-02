de82dfc4bf1877ee4e5da42f55050b5d6a9c2d30
3rick <erick.dejesus1@alumno.buap.mx>
2022-12-01 19:00:12 -0600

    add: destroy every comment once an article is removed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   # title must be present
   validates :title, presence: true
   # body must be present
